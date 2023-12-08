88b045bd760187b9c15262d59015571b6cdfb69d
Alexey Borohvostov <troizet@gmail.com>
2023-12-08 13:38:04 +0700

    added controllers

---
+++ controllers/MessageController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace app\controllers;
+
+use yii\rest\Controller;
+
+class MessageController extends Controller
+{
+    public function actionIndex()
+    {
+
+    }
+
+    public function actionView()
+    {
+
+    }
+
+    public function actionCreate()
+    {
+
+    }
+
+    public function actionUpdate()
+    {
+
+    }
+
+    public function actionDelete()
+    {
+
+    }
+
+
+
+}
---
+++ controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\controllers;
+
+use yii\rest\Controller;
+
+/**
+ * Делаем простую Basic авторизацию или jwt?
+ */
+class UserController extends Controller
+{
+
+    public function actionRegister()
+    {
+
+    }
+
+    public function actionLogin()
+    {
+
+    }
+
+    public function actionLogout()
+    {
+
+    }
+}
