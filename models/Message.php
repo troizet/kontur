4996d1f0b3d3fb6422b663d726f865fd0a6be9ae
Alexey Borohvostov <troizet@gmail.com>
2023-12-08 14:18:35 +0700

    added model

---
+++ models/Message.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\models;
+
+class Message
+{
+    /**
+     * message
+     * type: 1 - all, 2 - only registered user, 3 - with specify user
+     * from_user
+     * to_user
+     * created_at
+     * updated_at
+     */
+
+}
